d1526b87c3c0288ba1933a292a6afd36620df160
oguzhanozmenn <160310621+oguzhanozmenn@users.noreply.github.com>
2025-12-10 20:16:02 +0300

    Docker Compose ve Redis eklendi

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-# app.py
 from flask import Flask
-import os
+from redis import Redis
 
 app = Flask(__name__)
+# Buradaki "host='redis'" kısmı çok kritik!
+# Normalde 'localhost' yazarız ama Docker dünyasında servis ismini kullanacağız.
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    return "Merhaba Oguzhan! Bu benim ilk Dockerize edilmiş uygulamam ve değişikliği hadi görelim!"
+    try:
+        # Sayacı 1 arttır
+        count = redis.incr('hits')
+    except Exception as e:
+        return "Veritabanına bağlanılamadı! Redis çalışıyor mu?"
+
+    return f"Merhaba Oguzhan! Bu sayfayı şu ana kadar {count} kez görüntüledin."
 
 if __name__ == "__main__":
-    # Uygulamayı dışarıya açmak için host='0.0.0.0' kullanıyoruz
     app.run(host='0.0.0.0', port=5000)
